5ae2b9418e53491e4a92336f35ef8bae0e1e18f8
G-Mukha <79086648+G-Mukha@users.noreply.github.com>
2021-03-20 15:05:00 +0500

    Update bill_tip.py

--- bill_tip.py
+++ bill_tip.py
@@ -1,4 +1,5 @@
 # TIP CALCULATOR PROGRAM
+print("Welcome to the tip calculator!")
 
 total_bill = float(input("What was the total bill? $"))
 num_of_people = int(input("How many people to split the bill? "))
